9b422bcc3759089536f5eecf052da783d18d834d
urazakgul <urazdev@gmail.com>
2024-07-29 16:02:59 +0300

    updated

--- kod/statsbombpy_veri.py
+++
@@ -1,6 +0,0 @@
-# pip install statsbombpy
-from statsbombpy import sb
-
-competitions = sb.competitions()
-matches = sb.matches(competition_id=55, season_id=282)
-lineups = sb.lineups(match_id=3943043)
